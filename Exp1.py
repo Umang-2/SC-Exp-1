549d47a8fc30ddd1b418c5a9cdae496e402743fe
Umang <82362300+Umang-2@users.noreply.github.com>
2022-04-29 10:17:18 +0530

    Update Exp1.py

--- Exp1.py
+++ Exp1.py
@@ -6,8 +6,8 @@ import plotly.express as px
 st.title('Fuzzy Logic : Polynomial Membership Function')
 st.write("")
 a = st.slider('Value of a:', 1, 10)
-b = st.slider('Value of b:', 1, 10)
-c = st.slider('Value of c:', 1, 10)
+b = st.slider('Value of b:', 0, 10)
+c = st.slider('Value of c:', 0, 10)
 
 x = [i for i in range(-10,11)]
 
